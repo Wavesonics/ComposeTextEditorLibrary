22644e719327adfe47720e75799aa36516110db6
Adam Brown <adamwbrown@gmail.com>
2024-12-29 21:30:19 -0800

    Not needed

--- ComposeTextEditorSpellCheck/build.gradle.kts
+++ ComposeTextEditorSpellCheck/build.gradle.kts
@@ -50,7 +50,6 @@ kotlin {
 		}
 
 		val desktopMain by getting {
-			dependsOn(commonMain)
 			dependencies {
 				implementation(compose.desktop.currentOs)
 				implementation(libs.kotlinx.coroutines.swing)
